4d25916d3e921b848f4bf5b0d40c2653338cbc12
Danny Festor <danny.festor@gmail.com>
2022-07-07 19:40:46 +0900

    Make Snippet View

--- app/Http/Controllers/SnippetsController.php
+++ app/Http/Controllers/SnippetsController.php
@@ -9,9 +9,15 @@ class SnippetsController extends Controller
 {
     public function index()
     {
+        $snippets = Snippet::query()
+            ->with(['tags'])->select(['id', 'title', 'description'])
+            ->paginate();
+
+        return view('snippets.index', compact('snippets'));
     }
 
     public function show(Snippet $snippet)
     {
+        return view('snippets.show', compact('snippet'));
     }
 }
